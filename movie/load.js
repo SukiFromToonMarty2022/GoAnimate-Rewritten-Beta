7d0e6a1bdf3df5c9de963e84862844869045cbca
Joseph The Animator <73652344+josephcrosmanplays532@users.noreply.github.com>
2022-01-04 18:41:13 -0500

    lots of things to figure out.

--- movie/load.js
+++ movie/load.js
@@ -1,36 +1,17 @@
-const movie = require('./main');
+const caché = require('./caché');
+const fUtil = require('../fileUtil');
 const base = Buffer.alloc(1, 0);
+const fs = require('fs');
 
 module.exports = function (req, res, url) {
-	switch (req.method) {
-		case 'GET': {
-			const match = req.url.match(/\/movies\/([^.]+)(?:\.(zip|xml))?$/);
-			if (!match) return;
+	if (!url.path.startsWith('/goapi/getMovie')) return;
+	const zipF = fUtil.getFileIndex('movie-', '.xml', url.query.movieId);
+	res.setHeader('Content-Type', 'application/zip');
 
-			var id = match[1], ext = match[2];
-			switch (ext) {
-				case 'zip':
-					res.setHeader('Content-Type', 'application/zip');
-					movie.loadZip(id).then(v => { res.statusCode = 200, res.end(v) })
-						.catch(e => { res.statusCode = 404, res.end() })
-					break;
-				default:
-					res.setHeader('Content-Type', 'text/xml');
-					movie.loadXml(id).then(v => { res.statusCode = 200, res.end(v) })
-						.catch(e => { res.statusCode = 404, res.end() })
-			}
-			return true;
-		}
-
-		case 'POST': {
-			if (!url.path.startsWith('/goapi/getMovie/')) return;
-			res.setHeader('Content-Type', 'application/zip');
-
-			movie.loadZip(url.query.movieId).then(b =>
-				res.end(Buffer.concat([base, b]))
-			).catch(e => res.end('1'));
-			return true;
-		}
-		default: return;
-	}
+	caché.load(zipF).then(b => {
+		if (req.method == 'POST')
+			b = Buffer.concat([base, b]);
+		res.end(b);
+	});
+	return true;
 }
