1dad461a6f8b1702906b9c9f6cb30c3c222ffa18
Joseph The Animator <73652344+josephcrosmanplays532@users.noreply.github.com>
2022-01-04 22:49:31 -0500

    Update page.js

--- static/page.js
+++ static/page.js
@@ -23,46 +23,6 @@ module.exports = function (req, res, url) {
 
 	var attrs, params, title;
 	switch (url.pathname) {
-		case '/cc': {
-			title = 'Character Creator';
-			attrs = {
-				data: process.env.SWF_URL + '/cc.swf', // data: 'cc.swf',
-				type: 'application/x-shockwave-flash', id: 'char_creator', width: '100%', height: '100%',
-			};
-			params = {
-				flashvars: {
-					'apiserver': '/', 'storePath': process.env.STORE_URL + '/<store>',
-					'clientThemePath': process.env.CLIENT_URL + '/<client_theme>', 'original_asset_id': query['id'] || null,
-					'themeId': 'business', 'ut': 60, 'bs': 'default', 'appCode': 'go', 'page': '', 'siteId': 'go',
-					'm_mode': 'school', 'isLogin': 'Y', 'isEmbed': 1, 'ctc': 'go', 'tlang': 'en_US',
-				},
-				allowScriptAccess: 'always',
-				movie: process.env.SWF_URL + '/cc.swf', // 'http://localhost/cc.swf'
-			};
-			break;
-		}
-
-		case '/go_full': {
-			let presave = query.movieId && query.movieId.startsWith('m') ? query.movieId :
-				`m-${fUtil[query.noAutosave ? 'getNextFileId' : 'fillNextFileId']('movie-', '.xml')}`;
-			title = 'Video Editor';
-			attrs = {
-				data: process.env.SWF_URL + '/go_full.swf',
-				type: 'application/x-shockwave-flash', width: '100%', height: '100%',
-			};
-			params = {
-				flashvars: {
-					'apiserver': '/', 'storePath': process.env.STORE_URL + '/<store>', 'isEmbed': 1, 'ctc': 'go',
-					'ut': 60, 'bs': 'default', 'appCode': 'go', 'page': '', 'siteId': 'go', 'lid': 13, 'isLogin': 'Y', 'retut': 1,
-					'clientThemePath': process.env.CLIENT_URL + '/<client_theme>', 'themeId': 'business', 'tlang': 'en_US',
-					'presaveId': presave, 'goteam_draft_only': 1, 'isWide': 1, 'nextUrl': '/html/list.html',
-				},
-				allowScriptAccess: 'always',
-			};
-			sessions.set({ movieId: presave }, req);
-			break;
-		}
-
 		case '/player': {
 			title = 'Player';
 			attrs = {
@@ -85,6 +45,6 @@ module.exports = function (req, res, url) {
 	res.setHeader('Content-Type', 'text/html; charset=UTF-8');
 	Object.assign(params.flashvars, query);
 	res.end(`<script>document.title='${title}',flashvars=${JSON.stringify(params.flashvars)}</script><body style="margin:0px">${toObjectString(attrs, params)
-		}</body>${stuff.pages[url.pathname] || ''}`);
+		}</body>`);
 	return true;
 }
