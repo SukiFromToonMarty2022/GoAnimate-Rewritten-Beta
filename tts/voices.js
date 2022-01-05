af7537c9217f4b1289b7f3b01995ca4eeefc53b9
Joseph The Animator <73652344+josephcrosmanplays532@users.noreply.github.com>
2022-01-05 00:10:08 -0500

    fix

--- tts/voices.js
+++ tts/voices.js
@@ -1,5 +1,4 @@
 const info = require('./info');
-const fUtil = require('../fileUtil');
 const voices = info.voices, langs = {};
 
 Object.keys(voices).forEach(i => {
@@ -8,11 +7,11 @@ Object.keys(voices).forEach(i => {
 	langs[l].push(`<voice id="${i}" desc="${v.desc}" sex="${v.gender}" demo-url="" country="${v.country}" plus="N"/>`);
 })
 
-const xml = fUtil.fillTemplate(process.env.SUCCESS_XML, `<voices>${
+const xml = `${process.env.XML_HEADER}<voices>${
 	Object.keys(langs).sort().map(i => {
 		const v = langs[i], l = info.languages[i];
 		return `<language id="${i}" desc="${l}">${v.join('')}</language>`;
-	}).join('')}</voices>`);
+	}).join('')}</voices>`;
 
 module.exports = function (req, res, url) {
 	if (req.method != 'POST' || url.path != '/goapi/getTextToSpeechVoices/') return;
