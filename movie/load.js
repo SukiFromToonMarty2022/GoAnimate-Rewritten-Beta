fc869550775a460fd069300361b2a7175552c06d
Joseph The Animator <73652344+josephcrosmanplays532@users.noreply.github.com>
2022-01-04 19:20:36 -0500

    Update load.js

--- movie/load.js
+++ movie/load.js
@@ -26,14 +26,17 @@ module.exports = function (req, res, url) {
 		}
 
 		case 'POST': {
-			if (!url.path.startsWith('/goapi/getMovie/')) return;
+			if (!url.path.startsWith('/goapi/getMovie')) return;
+			const zipF = fUtil.getFileIndex('movie-', '.xml', url.query.movieId);
 			res.setHeader('Content-Type', 'application/zip');
-			var b = Buffer.concat([base, b]);
-			movie.loadZip(url.query.movieId).then(b =>
-				res.end(Buffer.concat([base, b])) 
-			).catch(e => res.end(b));
+			caché.load(zipF).then(b => {
+				if (req.method == 'POST')
+					b = Buffer.concat([base, b]);
+				res.end(b);
+			});
 			return true;
 		}
-		default: return;
+		default:
+			return;
 	}
-}
+};
