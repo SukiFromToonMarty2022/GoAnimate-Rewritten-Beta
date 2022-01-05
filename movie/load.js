bc140cbe31d93aafeb0fe59a5d792b1ccc1b3a64
Joseph The Animator <73652344+josephcrosmanplays532@users.noreply.github.com>
2022-01-04 19:25:21 -0500

    hm

--- movie/load.js
+++ movie/load.js
@@ -27,12 +27,11 @@ module.exports = function (req, res, url) {
 
 		case 'POST': {
 			if (!url.path.startsWith('/goapi/getMovie')) return;
-			const zipF = fUtil.getFileIndex('movie-', '.xml', url.query.movieId);
+
 			res.setHeader('Content-Type', 'application/zip');
-			caché.load(zipF).then(b => {
-				if (req.method == 'POST')
-					b = Buffer.concat([base, b]);
-				res.end(b);
+			caché.loadZip(url.query.movieId).then(b => {
+				b = Buffer.concat([base, b]);
+			res.end(b);
 			});
 			return true;
 		}
