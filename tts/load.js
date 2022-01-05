827757bc6c1bc5e56191301134bb02aa2e7c9cff
Joseph The Animator <73652344+josephcrosmanplays532@users.noreply.github.com>
2022-01-05 00:09:08 -0500

    Update load.js

--- tts/load.js
+++ tts/load.js
@@ -11,7 +11,7 @@ function processVoice(voiceName, text) {
 	return new Promise((res, rej) => {
 		const voice = voices[voiceName];
 		switch (voice.source) {
-			case 'polly': {
+			case 'polly':
 				var buffers = [];
 				var req = https.request({
 					hostname: 'pollyvoices.com',
@@ -25,42 +25,38 @@ function processVoice(voiceName, text) {
 					r.on('data', b => buffers.push(b));
 					r.on('end', () => {
 						var json = JSON.parse(Buffer.concat(buffers));
-						if (json.file) get(`https://pollyvoices.com${json.file}`).then(res);
-						else rej();
+						get(`https://pollyvoices.com${json.file}`).then(res);
 					});
 				});
 				req.write(qs.encode({ text: text, voice: voice.arg }));
 				req.end();
 				break;
-			}
 			case 'cepstral':
-			case 'voiceforge': {
-				https.get('https://www.voiceforge.com/demo', r => {
+				https.get('https://www.cepstral.com/en/demos', r => {
 					const cookie = r.headers['set-cookie'];
 					var q = qs.encode({
 						voice: voice.arg,
 						voiceText: text,
+						rate: 170,
+						pitch: 1,
+						sfx: 'none',
 					});
 					var buffers = [];
 					var req = https.get({
-						host: 'www.voiceforge.com',
+						host: 'www.cepstral.com',
 						path: `/demos/createAudio.php?${q}`,
 						headers: { Cookie: cookie },
 						method: 'GET',
 					}, r => {
 						r.on('data', b => buffers.push(b));
 						r.on('end', () => {
-							const html = Buffer.concat(buffers);
-							const beg = html.indexOf('id="mp3Source" src="') + 20;
-							const end = html.indexOf('"', beg);
-							const loc = html.subarray(beg, end).toString();
-							get(`https://www.voiceforge.com${loc}`).then(res).catch(rej);
+							var json = JSON.parse(Buffer.concat(buffers));
+							get(`https://www.cepstral.com${json.mp3_loc}`).then(res).catch(rej);
 						})
 					});
 				});
 				break;
-			}
-			case 'vocalware': {
+			case 'vocalware':
 				var q = qs.encode({
 					EID: voice.arg[0],
 					LID: voice.arg[1],
@@ -80,7 +76,7 @@ function processVoice(voiceName, text) {
 					headers: {
 						Referer: 'https://www.vocalware.com/index/demo',
 						Origin: 'https://www.vocalware.com',
-						'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36',
+						'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36'
 					},
 				}, r => {
 					var buffers = [];
@@ -88,69 +84,6 @@ function processVoice(voiceName, text) {
 					r.on('end', () => res(Buffer.concat(buffers)));
 					r.on('error', rej);
 				});
-				break;
-			}
-			case 'voicery': {
-				var q = qs.encode({
-					text: text,
-					speaker: voice.arg,
-					ssml: text.includes('<'),
-					//style: 'default',
-				});
-				https.get({
-					host: 'www.voicery.com',
-					path: `/api/generate?${q}`,
-				}, r => {
-					var buffers = [];
-					r.on('data', d => buffers.push(d));
-					r.on('end', () => res(Buffer.concat(buffers)));
-					r.on('error', rej);
-				});
-				break;
-			}
-			case 'watson': {
-				var q = qs.encode({
-					text: text,
-					voice: voice.arg,
-					download: true,
-					accept: "audio/mp3",
-				});
-				console.log(https.get({
-					host: 'text-to-speech-demo.ng.bluemix.net',
-					path: `/api/v1/synthesize?${q}`,
-				}, r => {
-					var buffers = [];
-					r.on('data', d => buffers.push(d));
-					r.on('end', () => res(Buffer.concat(buffers)));
-					r.on('error', rej);
-				}));
-				break;
-			}
-			case 'acapela': {
-				var q = qs.encode({
-					cl_login: "VAAS_MKT",
-					req_snd_type: "",
-					req_voice: voice.arg,
-					cl_app: "seriousbusiness",
-					req_text: text,
-					cl_pwd: "M5Awq9xu",
-				});
-				console.log(http.get({
-					host: 'vaassl3.acapela-group.com',
-					path: `/Services/AcapelaTV/Synthesizer?${q}`,
-					method: 'GET',
-				}, r => {
-					var buffers = [];
-					r.on('data', d => buffers.push(d));
-					r.on('end', () => {
-							const html = Buffer.concat(buffers);
-							const beg = html.indexOf('&snd_url=') + 9;
-							const end = html.indexOf('&', beg);
-                            const loc = `https${html.subarray(beg+4, end).toString()}`;
-                            get(loc).then(res).catch(rej);
-						})
-					r.on('error', rej);
-				}));
 				break;
 			}
 		}
