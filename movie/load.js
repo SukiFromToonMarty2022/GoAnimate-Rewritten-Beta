5e6ae1ac6eb59d565d7e0426e7f16f154f30981c
Joseph The Animator <73652344+josephcrosmanplays532@users.noreply.github.com>
2022-01-05 20:34:33 -0500

    Update load.js

--- movie/load.js
+++ movie/load.js
@@ -1,5 +1,8 @@
-const movie = require('./main');
+const caché = require('./caché');
+const fUtil = require('../fileUtil');
 const base = Buffer.alloc(1, 0);
+const fs = require('fs');
+const movie = require('./main'); 
 
 module.exports = function (req, res, url) {
 	switch (req.method) {
@@ -9,11 +12,6 @@ module.exports = function (req, res, url) {
 
 			var id = match[1], ext = match[2];
 			switch (ext) {
-				case 'zip':
-					res.setHeader('Content-Type', 'application/zip');
-					movie.loadZip(id).then(v => { res.statusCode = 200, res.end(v) })
-						.catch(e => { res.statusCode = 404, res.end() })
-					break;
 				default:
 					res.setHeader('Content-Type', 'text/xml');
 					movie.loadXml(id).then(v => { res.statusCode = 200, res.end(v) })
@@ -21,16 +19,18 @@ module.exports = function (req, res, url) {
 			}
 			return true;
 		}
-
-		case 'POST': {
+		case 'POST' : {
 			if (!url.path.startsWith('/goapi/getMovie/')) return;
+			const zipF = fUtil.getFileIndex('movie-', '.xml', url.query.movieId);
 			res.setHeader('Content-Type', 'application/zip');
-
-			movie.loadZip(url.query.movieId).then(b =>
-				res.end(Buffer.concat([base, b]))
-			).catch(e => res.end('1'));
+			
+			caché.load(zipF).then(b => {
+				b = Buffer.concat([base, b]);
+				res.end(b);
+			});
 			return true;
 		}
-		default: return;
+		default:
+			return;
 	}
-}
+};
