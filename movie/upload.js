98f6baf7454635bc11b0dd90a0a3455377fe0d85
Joseph The Animator <73652344+josephcrosmanplays532@users.noreply.github.com>
2022-01-05 20:46:50 -0500

    Update upload.js

--- movie/upload.js
+++ movie/upload.js
@@ -7,7 +7,7 @@ module.exports = function (req, res, url) {
 	if (req.method != 'POST' || url.path != '/upload_movie') return;
 	new formidable.IncomingForm().parse(req, (e, f, files) => {
 		const path = files.import.path, buffer = fs.readFileSync(path);
-		parse.unpackXml(buffer, numId);
+		parse.unpackXml(buffer);
 		fs.unlinkSync(path);
 
 		res.statusCode = 302;
