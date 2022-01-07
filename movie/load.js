28aca260618bb4c23c317cf1719d9a7a294b60b1
Joseph The Animator <73652344+josephcrosmanplays532@users.noreply.github.com>
2022-01-07 16:12:34 -0500

    Update load.js

--- movie/load.js
+++ movie/load.js
@@ -1,4 +1,4 @@
-const caché = require('./caché');
+const starter = require('./callMovie');
 const fUtil = require('../fileUtil');
 const base = Buffer.alloc(1, 0);
 const fs = require('fs');
@@ -21,11 +21,11 @@ module.exports = function (req, res, url) {
 		}
 		case 'POST' : {
 			if (!url.path.startsWith('/goapi/getMovie/')) return;
-			const zipF = fUtil.getFileIndex('movie-', '.xml', url.query.movieId);
 			res.setHeader('Content-Type', 'application/zip');
 			
-			caché.load(zipF).then(b => {
-				b = Buffer.concat([base, b]);
+			starter.load(url.query.movieId).then(b => {
+				if (req.method == 'POST')
+					b = Buffer.concat([base, b]);
 				res.end(b);
 			});
 			return true;
