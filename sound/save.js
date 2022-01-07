39abbeba87d1a40c3363d758554dc85e646b4f99
Joseph The Animator <73652344+josephcrosmanplays532@users.noreply.github.com>
2022-01-07 13:33:26 -0500

    Update save.js

--- sound/save.js
+++ sound/save.js
@@ -1,26 +1,18 @@
-const formidable = require("formidable");
-const sessions = require("../data/sessions");
-const asset = require("./main");
+const loadPost = require("../misc/post_body");
+const movie = require("../asset/main");
 const http = require("http");
-const fs = require("fs");
 
+/**
+ * @param {http.IncomingMessage} req
+ * @param {http.ServerResponse} res
+ * @param {string} url
+ * @returns {boolean}
+ */
 module.exports = function (req, res, url) {
-	if (req.method != "POST" || url.path != "/goapi/saveSound/") return;
+	if (req.method != "POST" || (url.path != "/goapi/saveSound/")) return;
 	loadPost(req, res).then((data) => {
-		tts(data.voice, data.text).then((buffer) => {
-			mp3Duration(buffer, (e, d) => {
-				var dur = d * 1e3;
-				if (e || !dur) {
-					return res.end(1 + util.xmlFail());
-				}
-
-				const title = `[${voices[data.voice].desc}] ${data.text}`;
-				const id = asset.save(buffer, data.presaveId, "voiceover", "mp3");
-				res.end(
-					`0<response><asset><id>${id}</id><enc_asset_id>${id}</enc_asset_id><type>sound</type><subtype>voiceover</subtype><title>${title}</title><published>0</published><tags></tags><duration>${dur}</duration><downloadtype>progressive</downloadtype><file>${id}</file></asset></response>`
-				);
-			});
-		});
+		var bytes = Buffer.from(data.bytes, "base64");
+		movie.save(bytes, data.presaveId, "voiceover", "mp3");
 	});
 	return true;
 };
