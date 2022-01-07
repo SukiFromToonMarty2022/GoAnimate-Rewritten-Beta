f7db0131f399db57947b180a5edf99163c5b3c7f
Joseph The Animator <73652344+josephcrosmanplays532@users.noreply.github.com>
2022-01-07 16:10:32 -0500

    Update save-starter.js

--- movie/save-starter.js
+++ movie/save-starter.js
@@ -1,17 +1,16 @@
 const loadPost = require('../request/post_body');
-const caché = require('./caché');
-const fUtil = require('../fileUtil');
+const movie = require('./callMovie');
 
 module.exports = function (req, res, url) {
 	if (req.method != 'POST' || url.path != '/goapi/saveTemplate/') return;
 	loadPost(req, res).then(data => {
-		var fn, id;
-		if (data.movieId)
-			fn = fUtil.getFileIndex('movie-', '.xml', data.movieId, 7), id = data.movieId;
-		else
-			fn = fUtil.getNextFile('movie-', '.xml', 7), id = fn.substr(fn.lastIndexOf('-') + 1, 7);
-		const body = Buffer.from(data.body_zip, 'base64');
-		caché.save(fn, body).then(() => res.end('0' + id));
+		if (data.is_triggered_by_autosave && data.noAutosave)
+			return res.end('0');
+		var id = data.movieId || data.presaveId;
+		var body = Buffer.from(data.body_zip, 'base64');
+		var thumb = data.thumbnail_large &&
+			Buffer.from(data.thumbnail_large, 'base64');
+		movie.save(body, thumb, id).then(nId => res.end('0' + nId));
 	});
 	return true;
 }
