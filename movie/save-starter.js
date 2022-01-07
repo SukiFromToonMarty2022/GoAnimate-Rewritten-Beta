220820670f40f2f188e14972c133868c1a5f79aa
Joseph The Animator <73652344+josephcrosmanplays532@users.noreply.github.com>
2022-01-07 16:23:22 -0500

    Update save-starter.js

--- movie/save-starter.js
+++ movie/save-starter.js
@@ -4,13 +4,9 @@ const starter = require('./callMovie');
 module.exports = function (req, res, url) {
 	if (req.method != 'POST' || url.path != '/goapi/saveTemplate/') return;
 	loadPost(req, res).then(data => {
-		if (data.is_triggered_by_autosave && data.noAutosave)
-			return res.end('0');
 		var id = data.movieId || data.presaveId;
 		var body = Buffer.from(data.body_zip, 'base64');
-		var thumb = data.thumbnail_large &&
-			Buffer.from(data.thumbnail_large, 'base64');
-		starter.save(body, thumb, id).then(nId => res.end('0' + nId));
+		starter.save(body, id).then(() => res.end('0' + id));
 	});
 	return true;
 }
