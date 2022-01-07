2ddb6b6c503a1fc826b5e65da7d9c0222c53aada
Joseph The Animator <73652344+josephcrosmanplays532@users.noreply.github.com>
2022-01-06 20:10:50 -0500

    Create ajax.js

---
+++ ajax.js
@@ -0,0 +1,17 @@
+const movie = require('./movie/main');
+const start = '/events/';
+module.exports = function (req, res, url) {
+	if (!url.path.startsWith(start)) return;
+	switch (url.path.substr(start.length)) {
+		case 'close': {
+			sessions.remove(req);
+			break;
+		}
+		default: {
+			res.end();
+			return false;
+		};
+	}
+	res.end();
+	return true;
+}
