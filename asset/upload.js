641c32b8bf580b3fd72388619c56da0fb9d8eb5c
Joseph The Animator <73652344+josephcrosmanplays532@users.noreply.github.com>
2022-01-07 12:57:32 -0500

    Update upload.js

--- asset/upload.js
+++ asset/upload.js
@@ -1,5 +1,5 @@
+const loadPost = require("../misc/post_body");
 const formidable = require("formidable");
-const sessions = require("../data/sessions");
 const asset = require("./main");
 const http = require("http");
 const fs = require("fs");
@@ -7,20 +7,46 @@ const fs = require("fs");
 /**
  * @param {http.IncomingMessage} req
  * @param {http.ServerResponse} res
- * @param {string} url
+ * @param {import("url").UrlWithParsedQuery} url
  * @returns {boolean}
  */
 module.exports = function (req, res, url) {
-	if (req.method != "POST" || url.path != "/upload_asset") return;
-	formidable().parse(req, (_, fields, files) => {
-		var [mode, ext] = fields.params.split(".");
-		var path = files.import.path;
-		var buffer = fs.readFileSync(path);
-		var mId = sessions.get(req).movieId;
-		asset.save(buffer, mId, mode, ext);
-		fs.unlinkSync(path);
-		delete buffer;
-		res.end();
-	});
-	return true;
+	if (req.method != "POST") return;
+	switch (url.pathname) {
+		case "/upload_asset":
+			formidable().parse(req, (_, fields, files) => {
+				var [mId, mode, ext] = fields.params.split(".");
+				switch (mode) {
+					case "vo":
+						mode = "voiceover";
+						break;
+					case "se":
+						mode = "soundeffect";
+						break;
+					case "mu":
+						mode = "music";
+						break;
+				}
+
+				var path = files.import.path;
+				var buffer = fs.readFileSync(path);
+				asset.save(buffer, mId, mode, ext);
+				fs.unlinkSync(path);
+				delete buffer;
+				res.end();
+			});
+			return true;
+		case "/goapi/saveSound/":
+			loadPost(req, res).then(([data, mId]) => {
+				var bytes = Buffer.from(data.bytes, "base64");
+				asset.save(bytes, mId, "voiceover", "ogg");
+			});
+			return true;
+		case "/goapi/saveTemplate/":
+			loadPost(req, res).then(([data, mId]) => {
+				var body = Buffer.from(data.body_zip, "base64");
+				res.end("0" + asset.save(body, mId, "starter", "xml"));
+			});
+			return true;
+	}
 };
