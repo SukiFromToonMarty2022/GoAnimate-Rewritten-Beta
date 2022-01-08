cb1481b826edc6f5e3e70b3c3374a75f5cf2de8a
Joseph The Animator <73652344+josephcrosmanplays532@users.noreply.github.com>
2022-01-08 00:23:22 -0500

    Update load.js

--- movie/load.js
+++ movie/load.js
@@ -36,7 +36,6 @@ module.exports = function (req, res, url) {
 			if (!url.path.startsWith("/goapi/getMovie/?movieId="))`${vid}`("&userId=null&ut=60") {
 				res.setHeader("Content-Type", "application/zip");
 				movie.loadZip(url.query.movieId).then((b) => res.end(Buffer.concat([base, b]))).catch(() => res.end("1"));
-			return true;
 			}
 		}
 			
