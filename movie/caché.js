582e977c29fc6e48fa761ea31dc866ede435adc1
Joseph The Animator <73652344+josephcrosmanplays532@users.noreply.github.com>
2022-01-08 11:00:13 -0500

    Ok

--- movie/caché.js
+++ movie/caché.js
@@ -21,89 +21,34 @@ function generateId() {
 	return id;
 }
 
-async function unpackMovie(zipFile, thumb = null, assetBuffers = null) {
-	return new Promise((res) => {
-		var pieces = [];
-		var stream = zipFile["movie.xml"].toReadStream();
-		stream.on("data", (b) => pieces.push(b));
-		stream.on("end", async () => {
-			var mainSlice = Buffer.concat(pieces).slice(0, -7);
-			var xmlBuffers = [];
-			var charBuffers = {};
-			var assetRemap = {};
+async function unpackMovie(path, buffer) {
+	return new Promise(res => {
+		const zip = nodezip.unzip(buffer);
 
-			// Remaps UGC asset IDs to match the current Wrapper environment.
-			for (let c = 0, end; ; c = mainSlice.indexOf("ugc.", c) + 4) {
-				if (c == 0) continue;
-				else if (c == 3) {
-					xmlBuffers.push(mainSlice.subarray(end));
-					break;
-				}
+		var data = '';
+		if (path != filePath)
+			cache = {}, filePath = path;
 
-				xmlBuffers.push(mainSlice.subarray(end, c));
-				end = mainSlice.indexOf("<", c + 1);
-				var assetId = mainSlice.subarray(c, end).toString();
-				var index = assetId.indexOf("-");
-				var prefix = assetId.substr(0, index);
-				switch (prefix) {
-					case "c": {
-						var t = new Date().getTime();
-						var dot = assetId.indexOf(".");
-						var charId = assetId.substr(0, dot);
-						var saveId = assetRemap[charId];
-						if (!assetRemap[charId]) {
-							saveId = assetRemap[charId] = `C-${~~(1e4 * Math.random())}-${t}`;
-						}
-
-						var remainder = assetId.substr(dot);
-						xmlBuffers.push(Buffer.from(saveId + remainder));
-						try {
-							charBuffers[saveId] = await char.load(charId);
-						} catch (e) {}
-						break;
+		const stream = zip['movie.xml'].toReadStream();
+		stream.on('data', b => data += b);
+		stream.on('end', () => {
+			var finalData = data.substr(0, data.length - 7);
+			var count = 0; for (var i in zip.entries()) count++;
+			if (count > 1)
+				for (var i in zip.entries()) {
+					var e = zip[i];
+					if (!e || e.name == 'movie.xml') {
+						count--; continue;
 					}
-					case "C": {
-						var dot = assetId.indexOf(".");
-						var charId = assetId.substr(0, dot);
-						charBuffers[charId] = await char.load(charId);
-						xmlBuffers.push(Buffer.from(assetId));
-						break;
-					}
-					default: {
-						xmlBuffers.push(Buffer.from(assetId));
-						break;
-					}
+					const chunks = [], str = e.toReadStream();
+					str.on('data', b => chunks.push(b));
+					str.on('end', () => {
+						finalData += `< asset name = "${e.name}" > ${Buffer.concat(chunks)}</asset > `;
+						if (!--count) fs.writeFile(path, finalData += `</film > `, res);
+					});
 				}
-			}
-
-			// Appends base-64 encoded assets into XML.
-			if (assetBuffers)
-				for (let aId in assetBuffers) {
-					var dot = aId.lastIndexOf(".");
-					var dash = aId.lastIndexOf("-");
-					//var mode = aId.substr(dash + 1, dot - dash - 1);
-
-					if (useBase64(aId)) {
-						var assetString = assetBuffers[aId].toString("base64");
-						xmlBuffers.push(Buffer.from(`<asset id="${aId}">${assetString}</asset>`));
-					} else xmlBuffers.push(Buffer.from(`<asset id="${aId}">${assetBuffers[aId]}</asset>`));
-				}
-
-			var hLen = header.length;
-			for (let id in charBuffers) {
-				var buff = charBuffers[id];
-				var hasHeader = buff.subarray(0, hLen / 2).toString() == header.substr(0, hLen / 2);
-				var start = buff.includes("file_name") ? buff.indexOf(".xml") + 6 : hasHeader ? hLen + 9 : 9;
-				xmlBuffers.push(Buffer.from(`<cc_char file_name='ugc.char.${id}.xml' x${buff.subarray(start)}`));
-			}
-
-			if (thumb) {
-				var thumbString = thumb.toString("base64");
-				xmlBuffers.push(Buffer.from(`<thumb>${thumbString}</thumb>`));
-			}
-
-			xmlBuffers.push(Buffer.from(`</film>`));
-			res(Buffer.concat(xmlBuffers));
+			else
+				fs.writeFile(path, finalData += `</film > `, res);
 		});
 	});
 }
@@ -145,7 +90,7 @@ module.exports = {
 					fs.writeFile(path, finalData += `</film > `, res);
 			});
 		});
-	},
+	},	
 	addAsset(buffer) {
 		const id = generateId();
 		cache[id] = buffer;
