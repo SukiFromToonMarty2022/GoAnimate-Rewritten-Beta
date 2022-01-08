3ec8edc69b1c8ba57addde8479f818bba2ac7b6d
Joseph The Animator <73652344+josephcrosmanplays532@users.noreply.github.com>
2022-01-08 01:23:18 -0500

    Update load.js

--- movie/load.js
+++ movie/load.js
@@ -1,6 +1,5 @@
 const caché = require('./caché');
 const fUtil = require('./fileUtil');
-const vPlay = require('./load-Vplayer');
 const base = Buffer.alloc(1, 0);
 const fs = require('fs');
 const movie = require('./main'); 
