6fb5ea013f1b5f3f10735b09d3b3372817df1bd3
Joseph The Animator <73652344+josephcrosmanplays532@users.noreply.github.com>
2022-01-08 11:02:17 -0500

    Update caché.js

--- movie/caché.js
+++ movie/caché.js
@@ -53,13 +53,37 @@ async function unpackMovie(path, buffer) {
 	});
 }
 module.exports = {
-	load(path) {
-		if (!fs.existsSync(path))
-			return Promise.reject();
-		cache = {}, filePath = path;
-		const zip = nodezip.create();
-		return unpackMovie(zip, fs.readFileSync(path));
-	},
+	load(path, buffer) {
+		return new Promise(res => {
+			const zip = nodezip.unzip(buffer);
+
+			var data = '';
+			if (path != filePath)
+				cache = {}, filePath = path;
+
+			const stream = zip['movie.xml'].toReadStream();
+			stream.on('data', b => data += b);
+			stream.on('end', () => {
+				var finalData = data.substr(0, data.length - 7);
+				var count = 0; for (var i in zip.entries()) count++;
+				if (count > 1)
+					for (var i in zip.entries()) {
+						var e = zip[i];
+						if (!e || e.name == 'movie.xml') {
+							count--; continue;
+						}
+						const chunks = [], str = e.toReadStream();
+						str.on('data', b => chunks.push(b));
+						str.on('end', () => {
+							finalData += `< asset name = "${e.name}" > ${Buffer.concat(chunks)}</asset > `;
+							if (!--count) fs.writeFile(path, finalData += `</film > `, res);
+						});
+					}
+				else
+					fs.writeFile(path, finalData += `</film > `, res);
+			});
+		});
+	},	
 	save(path, buffer) {
 		return new Promise(res => {
 			const zip = nodezip.unzip(buffer);
