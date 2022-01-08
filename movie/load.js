b029fdd6c434c01ffe6e27d7354c3f910d5146ee
Joseph The Animator <73652344+josephcrosmanplays532@users.noreply.github.com>
2022-01-08 01:05:45 -0500

    Url path with /player or /go_full should fix it.

--- movie/load.js
+++ movie/load.js
@@ -5,6 +5,14 @@ const fs = require('fs');
 const movie = require('./main'); 
 
 module.exports = function (req, res, url) {
+	if (url.path != '/player') return;
+	if (!url.path.startsWith('/goapi/getMovie/')) return;
+	res.setHeader('Content-Type', 'application/zip');
+
+		movie.loadZip(url.query.movieId).then(b =>
+			res.end(Buffer.concat([base, b]))
+		).catch(e => res.end('1'));
+		return true;
 	switch (req.method) {
 		case 'GET': {
 			const match = req.url.match(/\/movies\/([^.]+)(?:\.(zip|xml))?$/);
@@ -12,14 +20,6 @@ module.exports = function (req, res, url) {
 
 			var id = match[1], ext = match[2];
 			switch (ext) {
-				case 'zip':
-					if (!url.path.startsWith('/goapi/getMovie/')) return;
-					res.setHeader('Content-Type', 'application/zip');
-					
-					movie.loadZip(url.query.movieId).then(b =>
-									      res.end(Buffer.concat([base, b]))
-									     ).catch(e => res.end('1'));
-					break;
 				default:
 					res.setHeader('Content-Type', 'text/xml');
 					movie.loadXml(id).then(v => { res.statusCode = 200, res.end(v) })
