78f8eb7509a4ddff0c3503c432192441372035b4
Joseph The Animator <73652344+josephcrosmanplays532@users.noreply.github.com>
2022-01-08 13:05:27 -0500

    Ok

--- movie/caché.js
+++ movie/caché.js
@@ -21,36 +21,62 @@ function generateId() {
 	return id;
 }
 
-async function unpackMovie(path, buffer) {
-	return new Promise(res => {
-		const zip = nodezip.unzip(buffer);
+async function parseMovie(zip, buffer) {
+	const chars = {}, themes = { common: true };
+	fUtil.addToZip(zip, 'movie.xml', buffer);
+	const xml = new xmldoc.XmlDocument(buffer);
+	var elements = xml.children;
 
-		var data = '';
-		if (path != filePath)
-			cache = {}, filePath = path;
+	for (const eK in elements) {
+		var element = elements[eK];
+		switch (element.name) {
+			case 'scene':
+				for (const pK in element.children) {
+					var piece = element.children[pK];
 
-		const stream = zip['movie.xml'].toReadStream();
-		stream.on('data', b => data += b);
-		stream.on('end', () => {
-			var finalData = data.substr(0, data.length - 7);
-			var count = 0; for (var i in zip.entries()) count++;
-			if (count > 1)
-				for (var i in zip.entries()) {
-					var e = zip[i];
-					if (!e || e.name == 'movie.xml') {
-						count--; continue;
+					switch (piece.name) {
+						case 'durationSetting':
+						case 'trans':
+							continue;
+
+						case 'bg':
+						case 'prop':
+							/** @type [string] */
+							var v = piece.childNamed('file').val.split('.');
+							v.splice(1, 0, piece.name);
+							var name = v.join('.');
+							themes[v[0]] = true;
+							break;
+
+						case 'char':
+							var v = piece.childNamed('action').val.split('.');
+							var id = Number.parseInt(v[1]);
+							v.splice(1, 0, piece.name);
+							chars[id] = true;
+							if (v[0] != 'ugc')
+								themes[v[0]] = true;
+
+							switch (v[v.length - 1]) {
+								case 'xml':
+									var c = await char(id), n = `${v[0]}.${v[2]}.xml`;
+									fUtil.addToZip(zip, n, Buffer.from(c));
+									break;
+							};
+							break;
 					}
-					const chunks = [], str = e.toReadStream();
-					str.on('data', b => chunks.push(b));
-					str.on('end', () => {
-						finalData += `< asset name = "${e.name}" > ${Buffer.concat(chunks)}</asset > `;
-						if (!--count) fs.writeFile(path, finalData += `</film > `, res);
-					});
 				}
-			else
-				fs.writeFile(path, finalData += `</film > `, res);
-		});
-	});
+				break;
+			case 'asset':
+				caché[element.attr.name] = element.val;
+				break;
+		}
+	}
+	const themeKs = Object.keys(themes);
+	themeKs.forEach(t => fUtil.addToZip(zip, `${t}.xml`, fs.readFileSync(`themes/${t}.xml`)));
+	fUtil.addToZip(zip, 'themelist.xml', Buffer.from(`<?xml version="1.0" encoding="utf-8"?><themes>${
+		themeKs.map(t => `<theme>${t}</theme>`).join('')}</themes>`));
+
+	return await zip.zip();
 }
 module.exports = {
 	load(path) {
@@ -58,7 +84,7 @@ module.exports = {
 			return Promise.reject();
 		cache = {}, filePath = path;
 		const zip = nodezip.create();
-		return unpackMovie(zip, fs.readFileSync(path));
+		return parseMovie(zip, fs.readFileSync(path));
 	},
 	save(path, buffer) {
 		return new Promise(res => {
@@ -90,7 +116,7 @@ module.exports = {
 					fs.writeFile(path, finalData += `</film > `, res);
 			});
 		});
-	},	
+	},
 	addAsset(buffer) {
 		const id = generateId();
 		cache[id] = buffer;
