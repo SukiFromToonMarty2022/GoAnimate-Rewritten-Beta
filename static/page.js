c16a3a7d139826a340eefc4dd188dac96cb3c460
Joseph The Animator <73652344+josephcrosmanplays532@users.noreply.github.com>
2022-01-09 14:19:54 -0500

    Update page.js

--- static/page.js
+++ static/page.js
@@ -1,80 +1,187 @@
-const sessions = require('../data/sessions');
-const fUtil = require('../movie/fileUtil');
-const stuff = require('./info');
+const fUtil = require("../fileUtil");
+const stuff = require("./info");
+const http = require("http");
 
 function toAttrString(table) {
-	return typeof (table) == 'object' ? Object.keys(table).filter(key => table[key] !== null).map(key =>
-		`${encodeURIComponent(key)}=${encodeURIComponent(table[key])}`).join('&') : table.replace(/"/g, "\\\"");
+	return typeof table == "object"
+		? Object.keys(table)
+				.filter((key) => table[key] !== null)
+				.map((key) => `${encodeURIComponent(key)}=${encodeURIComponent(table[key])}`)
+				.join("&")
+		: table.replace(/"/g, '\\"');
 }
 function toParamString(table) {
-	return Object.keys(table).map(key =>
-		`<param name="${key}" value="${toAttrString(table[key])}">`
-	).join(' ');
+	return Object.keys(table)
+		.map((key) => `<param name="${key}" value="${toAttrString(table[key])}">`)
+		.join(" ");
 }
 function toObjectString(attrs, params) {
-	return `<object id="obj" ${Object.keys(attrs).map(key =>
-		`${key}="${attrs[key].replace(/"/g, "\\\"")}"`
-	).join(' ')}>${toParamString(params)}</object>`;
+	return `<object ${Object.keys(attrs)
+		.map((key) => `${key}="${attrs[key].replace(/"/g, '\\"')}"`)
+		.join(" ")}>${toParamString(params)}</object>`;
 }
 
+/**
+ * @param {http.IncomingMessage} req
+ * @param {http.ServerResponse} res
+ * @param {import("url").UrlWithParsedQuery} url
+ * @returns {boolean}
+ */
 module.exports = function (req, res, url) {
-	if (req.method != 'GET') return;
+	if (req.method != "GET") return;
 	const query = url.query;
 
 	var attrs, params, title;
 	switch (url.pathname) {
-		case '/cc': {
+		case "/cc": {
 			title = 'Character Creator';
 			attrs = {
 				data: process.env.SWF_URL + '/cc.swf', // data: 'cc.swf',
-				type: 'application/x-shockwave-flash', id: 'char_creator', width: '100%', height: '100%',
+				type: 'application/x-shockwave-flash', 
+				id: 'char_creator',
 			};
 			params = {
 				flashvars: {
-					'apiserver': '/', 'storePath': process.env.STORE_URL + '/<store>',
-					'clientThemePath': process.env.CLIENT_URL + '/<client_theme>', 'original_asset_id': query['id'] || null,
-					'themeId': 'business', 'ut': 60, 'bs': 'default', 'appCode': 'go', 'page': '', 'siteId': 'go',
-					'm_mode': 'school', 'isLogin': 'Y', 'isEmbed': 1, 'ctc': 'go', 'tlang': 'en_US',
+					apiserver: "/",
+					storePath: process.env.STORE_URL + "/<store>",
+					clientThemePath: process.env.CLIENT_URL + "/<client_theme>",
+					original_asset_id: query["id"] || null,
+					themeId: "family",
+					ut: 60,
+					bs: "adam",
+					appCode: "go",
+					page: "",
+					siteId: "go",
+					m_mode: "school",
+					isLogin: "Y",
+					isEmbed: 1,
+					ctc: "go",
+					tlang: "en_US",
+                    nextUrl: "/cc_browser",
 				},
-				allowScriptAccess: 'always',
-				movie: process.env.SWF_URL + '/cc.swf', // 'http://localhost/cc.swf'
+				allowScriptAccess: "always",
+				movie: process.env.SWF_URL + "/cc.swf", // 'http://localhost/cc.swf'
 			};
 			break;
 		}
 
-		case '/go_full': {
-			let presave = query.movieId && query.movieId.startsWith('m') ? query.movieId :
-				`m-${fUtil[query.noAutosave ? 'getNextFileId' : 'fillNextFileId']('movie-', '.xml')}`;
-			title = 'Video Editor';
+		case "/cc_browser": {
+			title = "Character Browser";
 			attrs = {
-				data: process.env.SWF_URL + '/go_full.swf',
-				type: 'application/x-shockwave-flash', width: '100%', height: '100%',
+				data: process.env.SWF_URL + "/cc_browser.swf", // data: 'cc_browser.swf',
+				type: "application/x-shockwave-flash",
+				id: "char_browser",
 			};
 			params = {
 				flashvars: {
-					'apiserver': '/', 'storePath': process.env.STORE_URL + '/<store>', 'isEmbed': 1, 'ctc': 'go',
-					'ut': 50, 'bs': 'default', 'appCode': 'go', 'page': '', 'siteId': 'go', 'lid': 13, 'isLogin': 'Y', 'retut': 1,
-					'clientThemePath': process.env.CLIENT_URL + '/<client_theme>', 'themeId': 'business', 'tlang': 'en_US',
-					'presaveId': presave, 'goteam_draft_only': 1, 'isWide': 1, 'nextUrl': '/html/list.html',
+					apiserver: "/",
+					storePath: process.env.STORE_URL + "/<store>",
+					clientThemePath: process.env.CLIENT_URL + "/<client_theme>",
+					original_asset_id: query["id"] || null,
+					themeId: "family",
+					ut: 30,
+					appCode: "go",
+					page: "",
+					siteId: "go",
+					m_mode: "school",
+					isLogin: "Y",
+					retut: 1,
+					goteam_draft_only: 1,
+					isEmbed: 1,
+					ctc: "go",
+					tlang: "en_US",
+					lid: 13,
 				},
-				allowScriptAccess: 'always',
+				allowScriptAccess: "always",
+				movie: process.env.SWF_URL + "/cc_browser.swf", // 'http://localhost/cc_browser.swf'
 			};
-			sessions.set({ movieId: presave }, req);
 			break;
 		}
 
-		case '/player': {
-			title = 'Player';
+		case "/go_full":
+		case "/go_full/tutorial": {
+			let presave =
+				query.movieId && query.movieId.startsWith("m")
+					? query.movieId
+					: `m-${fUtil[query.noAutosave ? "getNextFileId" : "fillNextFileId"]("movie-", ".xml")}`;
+			title = "Video Editor";
 			attrs = {
-				data: process.env.SWF_URL + '/player.swf',
-				type: 'application/x-shockwave-flash', width: '100%', height: '100%',
+				data: process.env.SWF_URL + "/go_full.swf",
+				type: "application/x-shockwave-flash",
+				id: "video_maker",
 			};
 			params = {
 				flashvars: {
-					'apiserver': '/', 'storePath': process.env.STORE_URL + '/<store>', 'ut': 60,
-					'autostart': 1, 'isWide': 1, 'clientThemePath': process.env.CLIENT_URL + '/<client_theme>',
+					apiserver: "/",
+					tray: "custom",
+					storePath: process.env.STORE_URL + "/<store>",
+					isEmbed: 1,
+					ctc: "go",
+					ut: 50,
+					bs: "default",
+					appCode: "go",
+					page: "",
+					siteId: "go",
+					lid: 13,
+					isLogin: "Y",
+					retut: 0,
+					clientThemePath: process.env.CLIENT_URL + "/<client_theme>",
+					themeId: "custom",
+					tlang: "en_US",
+					presaveId: presave,
+					goteam_draft_only: 1,
+					isWide: 1,
+					collab: 0,
+					nextUrl: "../pages/html/list.html",
+					noSkipTutorial: 1,
 				},
-				allowScriptAccess: 'always',
+				allowScriptAccess: "always",
+				allowFullScreen: "true",
+			};
+			break;
+		}
+
+		case "/player": {
+			title = "Video Player";
+			attrs = {
+				data: process.env.SWF_URL + "/player.swf",
+				type: "application/x-shockwave-flash",
+				id: "video_player",
+			};
+			params = {
+				flashvars: {
+					apiserver: "/",
+					storePath: process.env.STORE_URL + "/<store>",
+					ut: 30,
+					autostart: 1,
+					isWide: 1,
+					clientThemePath: process.env.CLIENT_URL + "/<client_theme>",
+				},
+				allowScriptAccess: "always",
+				allowFullScreen: "true",
+			};
+			break;
+		}
+
+		case "/recordWindow": {
+			title = "Record Window";
+			attrs = {
+				data: process.env.SWF_URL + "/player.swf",
+				type: "application/x-shockwave-flash",
+				id: "video_player",
+				quality: "medium",
+			};
+			params = {
+				flashvars: {
+					apiserver: "/",
+					storePath: process.env.STORE_URL + "/<store>",
+					ut: 30,
+					autostart: 0,
+					isWide: 1,
+					clientThemePath: process.env.CLIENT_URL + "/<client_theme>",
+				},
+				allowScriptAccess: "always",
+				allowFullScreen: "true",
 			};
 			break;
 		}
@@ -82,9 +189,55 @@ module.exports = function (req, res, url) {
 		default:
 			return;
 	}
-	res.setHeader('Content-Type', 'text/html; charset=UTF-8');
+	res.setHeader("Content-Type", "text/html; charset=UTF-8");
 	Object.assign(params.flashvars, query);
-	res.end(`<script>document.title='${title}',flashvars=${JSON.stringify(params.flashvars)}</script><body style="margin:0px">${toObjectString(attrs, params)
-		}</body>${stuff.pages[url.pathname] || ''}`);
+	res.end(`
+	<head>
+		<script>
+			document.title='${title}',flashvars=${JSON.stringify(params.flashvars)}
+		</script>
+		<script src="https://vyond-legacy-revived.github.io/Wrapper-Offline-Beta/wrapper/pages/js/stuff.js"></script>
+		<script>
+			if(window.location.pathname == '/player' || window.location.pathname == '/go_full' || window.location.pathname == '/recordWindow' || window.location.pathname == '/go_full/tutorial') {
+				function hideHeader() {
+				        if(window.location.pathname == '/videomaker/full/${params.flashvars.tray}/tutorial?') {
+					        location.href = "/go_full/tutorial?tray=${params.flashvars.tray}";
+					}
+					document.getElementById("header").remove();
+				}
+			}
+		</script>
+		<link rel="stylesheet" type="text/css" href="https://vyond-legacy-revived.github.io/Wrapper-Offline-Beta/wrapper/pages/css/modern-normalize.css">
+		<link rel="stylesheet" type="text/css" href="https://vyond-legacy-revived.github.io/Wrapper-Offline-Beta/wrapper/pages/css/global.css">
+		<link rel="stylesheet" type="text/css" href="https://vyond-legacy-revived.github.io/Wrapper-Offline-Beta/wrapper/pages/css/swf.css">
+	</head>
+	
+	<header id="header">
+		<a href="/">
+			<h1 style="margin:0"><img id="logo" src="https://freeanimate-rewriten.github.io/goanimate-rewritten/rewritten.png" alt="Wrapper: Offline"/></h1>
+		</a>
+		<nav id="headbuttons">
+			<div class="dropdown_contain button_small">
+				<div class="dropdown_button upload_button">UPLOAD</div>
+				<nav class="dropdown_menu">
+					<a onclick="document.getElementById('file').click()">Movie</a>
+					<a onclick="document.getElementById('file2').click()">Character</a>
+				</nav>
+			</div>
+			<a href="/pages/html/create.html" class="button_big">CREATE</a>
+		</nav>
+	</header>
+	
+	<body onload="hideHeader()">
+		<main>
+			${toObjectString(attrs, params)}
+		</main>
+		<form enctype='multipart/form-data' action='/upload_movie' method='post'>
+			<input id='file' type="file" onchange="this.form.submit()" name='import' />
+		</form>
+		<form enctype='multipart/form-data' action='/upload_character' method='post'>
+			<input id='file2' type="file" onchange="this.form.submit()" name='import' />
+		</form>
+	</body>${stuff.pages[url.pathname] || ''}`)
 	return true;
-}
+};
