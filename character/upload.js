48cb562a61e4db1a517507ab78541971dbd6a827
Joseph The Animator <73652344+josephcrosmanplays532@users.noreply.github.com>
2022-01-09 14:59:28 -0500

    Update upload.js

--- character/upload.js
+++ character/upload.js
@@ -1,6 +1,6 @@
 const formidable = require('formidable');
 const parse = require('../movie/parse');
-const fUtil = require('../fileUtil');
+const fUtil = require('../misc/file');
 const fs = require('fs');
 
 module.exports = function (req, res, url) {
