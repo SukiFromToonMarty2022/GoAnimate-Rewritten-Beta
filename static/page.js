9dae3926e4e3571d4d4fa1ca19528797dc142da4
Joseph The Animator <73652344+josephcrosmanplays532@users.noreply.github.com>
2022-01-15 20:23:23 -0500

    Update page.js

--- static/page.js
+++ static/page.js
@@ -33,7 +33,7 @@ module.exports = function (req, res, url) {
 			};
 			params = {
 				flashvars: {
-					'animationPath': process.env.SWF_URL + '/go_full.swf',
+					'animationPath': process.env.SWF_URL + '/',
 					'apiserver': '/', 'storePath': process.env.STORE_URL + '/<store>', 'isEmbed': 1, 'ctc': 'go',
 					'ut': 60, 'bs': 'default', 'appCode': 'go', 'page': '', 'siteId': 'go', 'lid': 13, 'isLogin': 'Y', 'retut': 1,
 					'clientThemePath': process.env.CLIENT_URL + '/<client_theme>', 'themeId': 'business', 'tlang': 'en_US',
